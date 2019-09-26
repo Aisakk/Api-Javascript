a7343d81869554fd4b16b8c89211ff29d3853b9b
Aisak <santaellaisaac2@gmail.com>
2019-09-26 00:47:10 -0400

    Change Person

--- js/test.js
+++ js/test.js
@@ -11,8 +11,14 @@ const pullPerson = () =>{
     .then(data =>{
         return container.innerHTML = `
             <img src="${data.results[0].picture.large}" alt="picture" class="rounded-circle">
-            <h3>Hi my Name is: <br></h3>
-            <h4>${data.results[0].name.first}</h4>
+            <h3>Hi my name complete is: <br></h3>
+            <h4>${data.results[0].name.first} ${data.results[0].name.last} </h4>
+            <h3>My gender: <br></hr>
+            <h4>${data.results[0].gender}</h4>
+            <h3>Email: <br></hr>
+            <h4>${data.results[0].email}</h4>
+            <h3>My Location: <br></hr>
+            <h4>${data.results[0].location.city} ${data.results[0].location.state} ${data.results[0].location.street}</h4>
         `
     })
     
